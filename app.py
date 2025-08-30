e251ad0f37958664ba4fe81e0ed2211daccb7cc6
Ayush <ayush438500@gmail.com>
2025-08-31 02:08:03 +0530

    setup done

---
+++ app.py
@@ -0,0 +1,14 @@
+import nextcord
+from nextcord.ext import commands
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix = "!", intents = intents)
+
+@bot.event
+async def on_ready():
+  print(f'Logged in as {bot.user.name}')
+  
+if __name__ == '__main__':
+  bot.run('')
