8bd9f97c7e4ddad694bdbd2f400ec303469bfe9d
Ayush <ayush438500@gmail.com>
2025-08-31 02:16:19 +0530

    env system completed to run the bot

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os 
+from dotenv import load_dotenv
 import nextcord
 from nextcord.ext import commands
 
@@ -9,6 +11,9 @@ bot = commands.Bot(command_prefix = "!", intents = intents)
 @bot.event
 async def on_ready():
   print(f'Logged in as {bot.user.name}')
-  
+
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
 if __name__ == '__main__':
-  bot.run('')
+  bot.run(token)
