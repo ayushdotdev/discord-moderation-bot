c91e0137c73c4bccfd0c3d979a1394a1c067d80a
Ayush <ayush438500@gmail.com>
2025-09-05 19:17:18 +0530

    invite command created with a button

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,5 +1,6 @@
 import nextcord
 from nextcord.ext import commands
+from nextcord import ButtonStyle
 
 class misc(commands.Cog):
   def __init__(self,bot):
@@ -12,10 +13,17 @@ class misc(commands.Cog):
 
   @commands.command(name = "invite")
   async def invite(self,ctx):
+    view = nextcord.ui.View()
+    button = nextcord.ui.Button(
+      label="Invite Me 🚀", 
+      style=ButtonStyle.link, 
+      url = "https://discord.com/oauth2/authorize?client_id=1133450786895044688&permissions=8&integration_type=0&scope=bot"
+      )
+    view.add_item(button)
     await ctx.send(embed = nextcord.Embed(
       color = 0xff00c8,
-      description = ""
-      ))
+      description = "Thank you for your consideration\nClick the button to invite me:\n"
+      ),view = view)
 
 def setup(bot):
   bot.add_cog(misc(bot))
